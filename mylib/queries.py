95f6efdae6b0b46d0b10c32b52d0812b2276d8ac
Alex Bzdel <ab913@duke.edu>
2022-11-03 22:11:39 +0000

    added some more cursor stuff + table scaffolding

--- mylib/queries.py
+++ mylib/queries.py
@@ -2,3 +2,20 @@ import sqlite3
 
 connection = sqlite3.connect("mydb.db")
 cursor = connection.cursor()
+
+table = """create table if not exists college_admissions(
+        serial_no int PRIMARY_KEY,
+        gre int,
+        toefl int,
+        university_rating int,
+        sop float,
+        lor float,
+        cgpa float,
+        research int,
+        chance_of_admit float
+    )"""
+
+
+cursor.execute(table)
+
+cursor.execute("insert into table values ()")
